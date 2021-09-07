7686f6f5c9a4063ad8b7db814e53c1e23dec1486
Daniel-code666 <dmartinezcifuentes180@gmail.com>
2021-09-07 10:23:00 -0500

    añadí angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto-test';
+  numero1: number;
+  numero2: number;
+  resultado: number;
+
+  public operar(op: string): void
+  {
+    console.log(op);
+
+    switch(op)
+    {
+      case 's':
+        this.resultado = this.numero1 + this.numero2;
+        break;
+      case 'r':
+        this.resultado = this.numero1 - this.numero2;
+        break;
+      case 'm':
+        this.resultado = this.numero1 * this.numero2;
+        break;
+      case 'd':
+        this.resultado = this.numero1 / this.numero2;
+        break;
+      default:
+        this.resultado = 0;
+    }
+    console.log(this.resultado);
+  }
+
+  public teclear(e:any):void{
+    // console.log(e);
+    console.log('la tecla oprimida fue: ' + e.key);
+    console.log('la palabra al momento es: ' + e.target.value);
+  }
 }
